956423ba22faf263be0ded95dc7bef2f7a888833
SahithiSriVarshiniKaruparthi-art <Sahithi.Sri.Varshini.Karuparthi@ibm.com>
2026-04-23 12:34:26 +0530

    Clean up comments

--- LiDARapp/Core/Models/Models.swift
+++ LiDARapp/Core/Models/Models.swift
@@ -2,9 +2,6 @@
 //  Models.swift
 //  LiDARapp
 //
-//  Created by LiDAR Team on 2026
-//  Copyright © 2026 LiDAR Measurement App. All rights reserved.
-//
 //  Data models for the LiDAR app
 //
 
@@ -20,30 +17,20 @@ enum MediaType: String, Codable {
 
 // MARK: - Media Item Model
 /// Represents a single photo or video captured by the app
-/// This is the main data structure that holds all information about captured media
 struct MediaItem: Identifiable, Codable {
-    /// Unique identifier for this media item
+
     let id: UUID
-    
-    /// File name where the media is stored (e.g., "photo_123.jpg")
+
     let fileName: String
-    
-    /// Type of media - either photo or video
+
     let type: MediaType
-    
-    /// When this media was captured
+
     let createdAt: Date
-    
-    /// Whether this media has associated LiDAR depth data
-    /// This will be true only if captured on a LiDAR-capable device
+
     let hasLiDARData: Bool
-    
-    /// Optional file name for the depth data (e.g., "depth_123.json")
-    /// Only present if hasLiDARData is true
+
     let depthDataFileName: String?
-    
-    /// Array of measurements taken on this media
-    /// Starts empty and gets populated when user measures distances
+ 
     var measurements: [Measurement]
     
     /// Initializer with default values
@@ -74,15 +61,13 @@ enum MeasurementType: String, Codable {
 // MARK: - Measurement Model
 /// Represents a single measurement taken on a media item
 struct Measurement: Identifiable, Codable {
-    /// Unique identifier for this measurement
+
     let id: UUID
-    
-    /// Type of measurement (distance or bounding box)
+
     let type: MeasurementType
     
     /// The calculated value in meters
     /// For distance: the distance between two points
-    /// For bounding box: stored as width (we'll store height separately)
     let value: Double
     
     /// For bounding box measurements, stores the height in meters
@@ -92,8 +77,7 @@ struct Measurement: Identifiable, Codable {
     /// For distance: 2 points [start, end]
     /// For bounding box: 4 points [topLeft, topRight, bottomRight, bottomLeft]
     let screenPoints: [CGPoint]
-    
-    /// When this measurement was taken
+
     let createdAt: Date
     
     /// Human-readable description of the measurement
@@ -126,10 +110,9 @@ struct Measurement: Identifiable, Codable {
 // MARK: - Depth Point Model
 /// Represents a 3D point in space with depth information from LiDAR
 struct DepthPoint: Codable {
-    /// X coordinate in 3D space (meters)
+
     let x: Float
-    
-    /// Y coordinate in 3D space (meters)
+
     let y: Float
     
     /// Z coordinate (depth) in 3D space (meters)
@@ -149,16 +132,13 @@ struct DepthPoint: Codable {
 /// Container for depth data captured from LiDAR sensor
 /// This stores the 3D point cloud data
 struct DepthData: Codable {
-    /// Array of 3D points captured by the LiDAR sensor
+
     let points: [DepthPoint]
-    
-    /// Width of the depth map (in pixels)
+
     let width: Int
-    
-    /// Height of the depth map (in pixels)
+
     let height: Int
-    
-    /// When this depth data was captured
+
     let capturedAt: Date
     
     /// Get depth point at specific screen coordinates
