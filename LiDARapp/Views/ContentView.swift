5e8558391f545184240e2d070081e901c99b37d8
SahithiSriVarshiniKaruparthi-art <Sahithi.Sri.Varshini.Karuparthi@ibm.com>
2026-04-23 12:38:18 +0530

    Clean up

--- LiDARapp/Views/ContentView.swift
+++ LiDARapp/Views/ContentView.swift
@@ -2,9 +2,6 @@
 //  ContentView.swift
 //  LiDARapp
 //
-//  Created by LiDAR Team on 2026
-//  Copyright © 2026 LiDAR Measurement App. All rights reserved.
-//
 //  Main app interface with Camera and Gallery tabs
 //
 
@@ -13,7 +10,6 @@ import SwiftUI
 // MARK: - Content View
 /// Main app view with tab navigation
 struct ContentView: View {
-    // Track the selected tab
     @State private var selectedTab = 0
     
     var body: some View {
